faba51b917a895396c187475a2836ad227c83b47
Saruf Ratul <saruftoratul95@gmail.com>
2020-08-16 11:24:23 +0600

    first commit

---
+++ assets/js/kimj.js
@@ -0,0 +1,34 @@
+var paragraph = document.getElementsByTagName('p')[0];
+
+function textEffect(animationName) {
+  var text = paragraph.innerHTML,
+		chars = text.length,
+		newText = '',
+		animation = animationName,
+		char,
+		i;
+
+	for (i = 0; i < chars; i += 1) {
+		newText += '<i>' + text.charAt(i) + '</i>';
+	}
+
+	paragraph.innerHTML = newText;
+
+	var wrappedChars = document.getElementsByTagName('i'),
+		wrappedCharsLen = wrappedChars.length,
+		j = 0;
+
+	function addEffect () {
+		setTimeout(function () {
+			wrappedChars[j].className = animation;
+			j += 1;
+			if (j < wrappedCharsLen) {
+				addEffect();
+			}
+		}, 100)
+	}
+
+	addEffect();
+};
+
+textEffect('fly-in-out');
